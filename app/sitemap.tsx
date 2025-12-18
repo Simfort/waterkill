a59e8c6dddfb93d45682b69f48bdfd9e21cd5606
Simfort <lemondzavadavid265@gmail.com>
2025-12-18 20:02:51 +0300

    added icon

--- app/sitemap.tsx
+++ app/sitemap.tsx
@@ -12,6 +12,14 @@ export default function sitemap() {
       changeFrequency: "weekly",
       priority: 0.8,
     },
+    {
+      name: "waterkill",
+      short_name: "waterkill",
+      icons: [
+        { src: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+        { src: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      ],
+    },
     // ...другие страницы
   ];
 }
