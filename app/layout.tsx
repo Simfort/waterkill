bb21572d7b23b44b775645f1fc3672b18edbb92f
Simfort <lemondzavadavid265@gmail.com>
2025-12-19 21:44:30 +0300

    added yandex metric

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 
 import "./globals.css";
 import Footer from "./_components/Footer";
+import Script from "next/script";
 
 const InterFont = Inter({
   weight: ["400", "300", "500", "600", "700"],
@@ -49,6 +50,37 @@ export default function RootLayout({
         <div className="size-150 fixed -translate-x-27.5 top-0 bottom-0 left-0  rounded-full border -z-10 border-primary max-md:size-50"></div>
         {children} <Footer />
         <div className="h-150 w-150 -z-10 fixed translate-x-27.5 top-[45%] bottom-0 right-0  rounded-full border border-primary max-md:size-50"></div>{" "}
+        <Script
+          id="yandex-metrika"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+            m[i].l=1*new Date();
+            for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+            })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=105929081', 'ym');
+
+            ym(105929081, 'init', {
+              ssr: true,
+              webvisor: true,
+              clickmap: true,
+              ecommerce: "dataLayer",
+              accurateTrackBounce: true,
+              trackLinks: true
+            });
+          `,
+          }}
+        />
+        <noscript>
+          <div>
+            <img
+              src="https://mc.yandex.ru/watch/105929081"
+              className="absolute left-[-9999px]"
+              alt=""
+            />
+          </div>
+        </noscript>
       </body>
     </html>
   );
